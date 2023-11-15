ed761bc7808253d25aec8eea03003b320147dc97
NebieAlert <mjnn100801@naver.com>
2023-11-15 17:06:12 +0900

    CSS 수정

--- src/components/List/index.jsx
+++ src/components/List/index.jsx
@@ -67,7 +67,8 @@ const ListSectionTitle = styled.h1`
 
 const MessageBox = styled.div`
   width: 100%;
-  height: 40px;
+  padding-top: 0.5rem;
+  height: 35px;
   border-top: 1px solid #1f1f1f50;
   word-wrap: break-word;
   text-overflow: ellipsis;
@@ -75,7 +76,7 @@ const MessageBox = styled.div`
   overflow: hidden;
   display: -webkit-box;
   -webkit-box-orient: vertical;
-  -webkit-line-clamp: 2;
+  -webkit-line-clamp: 1;
 `;
 
 const FilterBtnContainer = styled.div`
